b6cb95dd2cd03c3bd999fd0dc1f745bdc89da77f
Wouter Termont <woutermont@gmail.com>
2023-11-27 17:22:46 +0100

    chore: fix indent issue
    Signed-off-by: Wouter Termont <woutermont@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        tsconfigRootDir: __dirname, // this is the reason this is a .js file
-        project: ['./tsconfig.eslint.json'],
-    },
-    extends: [
-        './dist/index.js'
-    ],
-    rules: {
-        // ESLint rules are kebab-case
-        '@typescript-eslint/naming-convention': 'off',
-    }
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname, // this is the reason this is a .js file
+    project: ['./tsconfig.eslint.json'],
+  },
+  extends: [
+    './dist/index.js'
+  ],
+  rules: {
+    // ESLint rules are kebab-case
+    '@typescript-eslint/naming-convention': 'off',
+  }
 };

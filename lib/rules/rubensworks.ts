c3b43de3d7b00f49a97add5cbd4daac296c144c3
Wouter Termont <woutermont@gmail.com>
2023-12-18 11:48:01 +0100

    chore: sort rules
    Signed-off-by: Wouter Termont <woutermont@gmail.com>

--- lib/rules/rubensworks.ts
+++ lib/rules/rubensworks.ts
@@ -1,62 +1,25 @@
 import type { Linter } from 'eslint';
 
 const js: Linter.RulesRecord = {
-
-  /* Default */
-
-  // Conflicts with functions from interfaces that sometimes don't require `this`
-  'class-methods-use-this': 'off',
-
-  '@stylistic/comma-dangle': [ 'error', 'always-multiline' ],
-  '@stylistic/dot-location': [ 'error', 'property' ],
-  '@stylistic/lines-between-class-members': [ 'error', 'always', {
-    exceptAfterSingleLine: true,
-  }],
-
-  // Conflicts with external libraries
-  'no-underscore-dangle': 'off',
-
-  '@stylistic/padding-line-between-statements': 'off',
-  'no-param-reassign': 'off',
+  'class-methods-use-this': 'off', // Conflicts with functions from interfaces that sometimes don't require `this`
+  'default-case': 'off', // TSC already takes care of these checks
   'func-style': 'off',
-  'new-cap': 'off',
-  '@stylistic/lines-around-comment': [ 'error', {
-    beforeBlockComment: false,
-    afterBlockComment: false,
-    beforeLineComment: false,
-    afterLineComment: false,
-  }],
-  'no-multi-assign': 'off',
-  'no-plusplus': 'off',
   'guard-for-in': 'off',
-
-  // Disabled in favor of eslint-plugin-import
-  'sort-imports': 'off',
-
-  'prefer-named-capture-group': 'off',
-  '@stylistic/max-len': [ 'error', {
-    code: 120,
-    ignoreTemplateLiterals: true,
-  }],
-  'unicorn/consistent-function-scoping': 'off',
-  'no-warning-comments': 'off',
-  '@stylistic/no-mixed-operators': 'off',
-  'prefer-destructuring': 'off',
-
-  // TSC already takes care of these checks
-  'default-case': 'off',
-
+  'new-cap': 'off',
+  'no-duplicate-imports': 'off', // Incompatible with type imports
   'no-loop-func': 'off',
-  'unicorn/no-array-callback-reference': 'off',
+  'no-multi-assign': 'off',
+  'no-param-reassign': 'off',
+  'no-plusplus': 'off',
+  'no-underscore-dangle': 'off', // Conflicts with external libraries
+  'no-warning-comments': 'off',
+  'prefer-destructuring': 'off',
+  'prefer-named-capture-group': 'off',
+  'sort-imports': 'off', // Disabled in favor of eslint-plugin-import
+  'unused-imports/no-unused-imports-ts': 'error',
+
   'extended/consistent-err-names': 'off',
-  'unicorn/prefer-string-replace-all': 'off',
-  'unicorn/catch-error-name': [ 'error', { name: 'error' }],
-  'unicorn/no-reduce': 'off',
 
-  // Incompatible with type imports
-  'no-duplicate-imports': 'off',
-
-  /* Import */
   'import/order': [ 'error', {
     alphabetize: {
       order: 'asc',
@@ -64,27 +27,52 @@ const js: Linter.RulesRecord = {
     },
   }],
   'import/no-unused-modules': 'off',
-  'unused-imports/no-unused-imports-ts': 'error',
   'import/no-extraneous-dependencies': 'error',
 
-  // TODO: Try to re-enable the following rules in the future
+  'unicorn/consistent-function-scoping': 'off',
+  'unicorn/no-array-callback-reference': 'off',
+  'unicorn/prefer-string-replace-all': 'off',
+  'unicorn/catch-error-name': [ 'error', { name: 'error' }],
+  'unicorn/no-reduce': 'off',
+
+  '@stylistic/comma-dangle': [ 'error', 'always-multiline' ],
+  '@stylistic/dot-location': [ 'error', 'property' ],
+  '@stylistic/lines-around-comment': [ 'error', {
+    beforeBlockComment: false,
+    afterBlockComment: false,
+    beforeLineComment: false,
+    afterLineComment: false,
+  }],
+  '@stylistic/lines-between-class-members': [ 'error', 'always', {
+    exceptAfterSingleLine: true,
+  }],
+  '@stylistic/max-len': [ 'error', {
+    code: 120,
+    ignoreTemplateLiterals: true,
+  }],
+  '@stylistic/no-mixed-operators': 'off',
+  '@stylistic/padding-line-between-statements': 'off',
+
+  /* TODO: Try to re-enable the following rules in the future */
 
   'global-require': 'off',
+
   'tsdoc/syntax': 'off',
+
   'unicorn/expiring-todo-comments': 'off',
   'unicorn/import-style': 'off',
-
   'unicorn/prefer-includes': 'off', // somehow raises additional errors now
   'unicorn/prefer-spread': 'off', // somehow raises additional errors now
+
   'import/no-nodejs-modules': 'off', // enabled in @rubensworks/eslint-config@v2.0.0
 
-  // TODO: should the following be enabled? They are ignored in some code files
+  /* TODO: should the following be enabled? They are ignored in some code files */
 
   'no-case-declarations': 'off',
   'no-console': 'off',
   'no-sync': 'off',
 
-  // TODO: evaluate the following added rules of thenativeweb
+  /* TODO: evaluate the following added rules of thenativeweb */
 
   'no-nonoctal-decimal-escape': 'off',
   'no-restricted-properties': 'off',
@@ -157,97 +145,95 @@ const ts: Linter.RulesRecord = {
     exceptAfterSingleLine: true,
     exceptAfterOverload: true,
   }],
-
-  // Breaks with default void in Asynchandler 2nd generic
-  '@typescript-eslint/no-invalid-void-type': 'off',
-
-  '@typescript-eslint/array-type': [ 'error', { default: 'array' }],
-  '@typescript-eslint/generic-type-naming': 'off',
-  '@typescript-eslint/no-empty-interface': 'off',
-
-  // Problems with optional parameters
-  '@typescript-eslint/no-unnecessary-condition': 'off',
-
+  '@stylistic/object-curly-spacing': 'off',
   '@stylistic/space-before-function-paren': [ 'error', 'never' ],
-  '@typescript-eslint/promise-function-async': 'off',
-  '@typescript-eslint/consistent-type-assertions': [ 'error', { assertionStyle: 'angle-bracket' }],
-  '@typescript-eslint/member-naming': 'off',
-  '@typescript-eslint/no-dynamic-delete': 'off',
+
+  '@typescript-eslint/array-type': [ 'error', {
+    default: 'array',
+  }],
+  '@typescript-eslint/consistent-type-assertions': [ 'error', {
+    assertionStyle: 'angle-bracket',
+  }],
+  '@typescript-eslint/consistent-type-imports': [ 'error', {
+    prefer: 'type-imports',
+  }],
   '@typescript-eslint/explicit-function-return-type': [ 'error', {
     allowExpressions: true,
     allowTypedFunctionExpressions: true,
     allowHigherOrderFunctions: true,
     allowConciseArrowFunctionExpressionsStartingWithVoid: true,
   }],
+  '@typescript-eslint/generic-type-naming': 'off',
+  '@typescript-eslint/member-naming': 'off',
+  '@typescript-eslint/no-dynamic-delete': 'off',
+  '@typescript-eslint/no-empty-interface': 'off',
+  '@typescript-eslint/no-invalid-void-type': 'off', // Breaks with default void in Asynchandler 2nd generic
+  '@typescript-eslint/no-unnecessary-condition': 'off', // Problems with optional parameters
   '@typescript-eslint/no-use-before-define': 'off',
   '@typescript-eslint/prefer-nullish-coalescing': 'off',
-  '@typescript-eslint/consistent-type-imports': [ 'error', { prefer: 'type-imports' }],
+  '@typescript-eslint/promise-function-async': 'off',
 
-  camelcase: 'off',
-  '@typescript-eslint/naming-convention': [
-    'error',
-    {
-      selector: 'interface',
-      format: [ 'PascalCase' ],
-      custom: {
-        regex: '^[A-Z]', // Was '^I[A-Z]' but overridden in projects
-        match: true,
-      },
+  'camelcase': 'off',
+  '@typescript-eslint/naming-convention': [ 'error', {
+    selector: 'interface',
+    format: [ 'PascalCase' ],
+    custom: {
+      regex: '^[A-Z]', // Was '^I[A-Z]' but overridden in projects
+      match: true,
     },
-  ],
+  }],
 
-  // TODO: Try to re-enable the following rules in the future
+  /* TODO: Try to re-enable the following rules in the future */
 
   '@typescript-eslint/no-require-imports': 'off',
-  '@typescript-eslint/no-var-requires': 'off',
-  '@typescript-eslint/no-unused-vars': 'off',
-
   '@typescript-eslint/no-unnecessary-type-assertion': 'off', // somehow raises additional errors now
+  '@typescript-eslint/no-unused-vars': 'off',
+  '@typescript-eslint/no-var-requires': 'off',
 
-  // TODO: evaluate the following added rules of thenativeweb
+  /* TODO: evaluate the following added rules of thenativeweb */
 
-  '@typescript-eslint/no-unsafe-argument': 'off',
   '@typescript-eslint/no-shadow': 'off',
+  '@typescript-eslint/no-unsafe-argument': 'off',
   '@typescript-eslint/non-nullable-type-assertion-style': 'off',
-  '@stylistic/object-curly-spacing': 'off',
 };
 
 const bin: Linter.RulesRecord = {
+  'no-process-env': 'off',
+
   'unicorn/filename-case': [ 'error', {
     case: 'kebabCase',
   }],
-  'no-process-env': 'off',
   'unicorn/no-process-exit': 'off', // disabled in @rubensworks v2
 };
 
 const test: Linter.RulesRecord = {
+  'arrow-body-style': 'off',
+  'id-length': 'off',
+  'line-comment-position': 'off',
+  'mocha/no-sibling-hooks': 'off',
   'mocha/no-synchronous-tests': 'off',
   'mocha/valid-test-description': 'off',
-  'mocha/no-sibling-hooks': 'off',
+  'no-inline-comments': 'off',
+  'no-new': 'off',
+  'no-return-assign': 'off',
+  'no-sync': 'off',
+  'no-useless-call': 'off',
 
   'import/no-extraneous-dependencies': 'off',
 
   'unicorn/filename-case': 'off',
   'unicorn/no-nested-ternary': 'off',
 
-  '@stylistic/max-statements-per-line': 'off',
-  'id-length': 'off',
-  'arrow-body-style': 'off',
-  'line-comment-position': 'off',
-  'no-inline-comments': 'off',
-  'no-new': 'off',
-  'no-return-assign': 'off',
-  'no-useless-call': 'off',
-  'no-sync': 'off',
-
   '@stylistic/brace-style': 'off',
+  '@stylistic/max-statements-per-line': 'off',
+  '@stylistic/no-extra-parens': 'off',
+
   '@typescript-eslint/ban-ts-comment': 'off',
   '@typescript-eslint/ban-ts-ignore': 'off',
   '@typescript-eslint/explicit-function-return-type': 'off',
-  '@typescript-eslint/unbound-method': 'off',
-  '@stylistic/no-extra-parens': 'off',
-  '@typescript-eslint/restrict-plus-operands': 'off',
   '@typescript-eslint/require-array-sort-compare': 'off',
+  '@typescript-eslint/restrict-plus-operands': 'off',
+  '@typescript-eslint/unbound-method': 'off',
 };
 
 export {
